547bdc814d77b3f97f909af6eefaf018d836cd2e
Nandeeka Nayak <nandeeka@gmail.com>
2019-11-17 11:08:22 -0800

    Update MCTS to include new StateNode interface

--- mcts.py
+++ mcts.py
@@ -26,39 +26,38 @@ def mcts(problem):
 #     # tree
 #     return
 
-def expand(state):
+def expand(node):
     """
     Expand unexplored child node. Fails if node has no more unexplored
     children.
     """
     # Get the next unexplored state
-    nextState = state.exploreChildNode()
+    nextState = node.exploreChildNode()
 
     # If all states are already explored, recurse
     if nextState is not None:
         return nextState
     else:
-        return expand(state.UCB1())
+        return expand(node.UCB1())
 
 
-def rollout(state, action):
+def rollout(problem, state):
     """
     Run a simulated playout from current state until it reaches a terminal
     state.
     """
-    while not state.isTerminal():
-        action = random.choice(state.getActions())
-        # take this action
-        # TODO: transition
-        state = t(state, action)  # ???
+    problem.setState(state)
+    while not problem.isTerminal():
+        action = random.choice(problem.legalMoves())
+        problem.move(action)
 
-    return state.getValue()
+    return problem.finalScore()
 
 
-def backprop(state, result):
+def backprop(node, result):
     """
     Update current move sequence with simulation result
     """
-    while state:
-        state.addOutcome(result)
-        state = state.getParent()
+    while node:
+        node.addOutcome(result)
+        node = node.getParent()
